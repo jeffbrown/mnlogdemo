17278810203857b9bd1bdfe2072fcadceeffe896
Jeff Scott Brown <brownj@objectcomputing.com>
2018-08-26 10:21:56 -0500

    simplify test

--- src/test/groovy/mnlogdemo/helpers/SomeHelperSpec.groovy
+++ src/test/groovy/mnlogdemo/helpers/SomeHelperSpec.groovy
@@ -1,23 +1,11 @@
 package mnlogdemo.helpers
 
-import io.micronaut.context.BeanContext
-import io.micronaut.context.BeanLocator
-import spock.lang.AutoCleanup
-import spock.lang.Shared
 import spock.lang.Specification
-
 
 class SomeHelperSpec extends Specification {
 
-    @Shared
-    @AutoCleanup
-    BeanLocator locator = BeanContext.run()
-
-    @Shared
-    SomeHelper helper = locator.getBean(SomeHelper)
-
     void 'test multiplication'() {
         expect:
-        28 == helper.multiply(4, 7)
+        28 == new SomeHelper().multiply(4, 7)
     }
 }
